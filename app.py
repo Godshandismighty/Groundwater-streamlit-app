5800babf4dfa673456e49e8cc2ff9ac625281d60
Jolaosho Oluwadamilare o. <oluwadamilarejolaosho@gmail.com>
2026-01-17 20:29:05 +0100

    app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(page_title="Groundwater Level Prediction")
+
+st.title("Groundwater Level Prediction App")
+
+st.write(
+    """
+    This Streamlit app is used for groundwater level prediction.
+    Model logic from the Jupyter notebook will be integrated here.
+    """
+)
+
+st.info("App setup successful. Streamlit is running correctly.")
